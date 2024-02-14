286bbd1b2cb1f03cfd36b631fb27b481ffcb5923
„royalvikingr“ <sebastian.prinz@posteo.de>
2024-02-14 16:39:32 +0100

    update main.tf for IGW creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,10 @@ resource "aws_vpc" "dev_vpc" {
     name     = "deham10"
   }
 }
+resource "aws_internet_gateway" "cpstn_IGW"{
+  # attach the igw to the following vpc
+  vpc_id                 = aws_vpc.dev_vpc.id
+  tags                   = {
+      Name               = "cpstn_vpc_igw"
+  }
+}
